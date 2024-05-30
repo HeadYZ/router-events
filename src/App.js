595173ceba8289d5e41cc1219c67dad7ce041673
Patryk <krasowski.p@outlook.com>
2024-05-30 17:34:57 +0200

    add nested layout for events pages

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import EventsPage from './pages/EventsPage.jsx'
 import EventDetailPage from './pages/EventDetailPage.jsx'
 import NewEventPage from './pages/NewEventPage.jsx'
 import EditEventPage from './pages/EditEventPage.jsx'
-
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import EventsLayout from './pages/EventsLayout.jsx'
 
 const router = createBrowserRouter([
 	{
@@ -15,10 +13,16 @@ const router = createBrowserRouter([
 		element: <RootLayout />,
 		children: [
 			{ index: true, element: <HomePage /> },
-			{ path: '/events', element: <EventsPage /> },
-			{ path: '/events/:eventId', element: <EventDetailPage /> },
-			{ path: '/events/new', element: <NewEventPage /> },
-			{ path: '/events/:eventId/edit', element: <EditEventPage /> },
+			{
+				path: 'events',
+				element: <EventsLayout />,
+				children: [
+					{ index: true, element: <EventsPage /> },
+					{ path: ':eventId', element: <EventDetailPage /> },
+					{ path: 'new', element: <NewEventPage /> },
+					{ path: ':eventId/edit', element: <EditEventPage /> },
+				],
+			},
 		],
 	},
 ])
