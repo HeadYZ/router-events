1ba9c24578e6f0cdf7b9a08e576f2e86b72fb6c8
Patryk <krasowski.p@outlook.com>
2024-06-02 20:06:10 +0200

    throw Response as error if !respone.ok, handling status erros

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,23 @@
- function ErrorPage(){
-    return <h1>Error occured !</h1>
- }
+import { useRouteError } from 'react-router-dom'
+import PageContent from './PageContent.jsx'
+import MainNavigation from '../components/MainNavigation.jsx'
+function ErrorPage() {
+	const error = useRouteError()
 
+	let title = 'An error occured!'
+	let message = 'Something went wrong!'
+	console.log(error)
+	if ((error.status === 404)) message = 'Could not find page.'
+	if ((error.status === 500)) message = JSON.parse(error.data).message
 
+	return (
+		<>
+			<MainNavigation />
+			<PageContent title={title}>
+				<p>{message}</p>
+			</PageContent>
+		</>
+	)
+}
 
- export default ErrorPage
+export default ErrorPage
