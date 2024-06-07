1cb1f8e8f915cf2ffefad4b26f18733dd2a56129
Patryk <krasowski.p@outlook.com>
2024-06-07 20:50:09 +0200

    change action to fetch data on more dunamically, create newsletter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from './pages/HomePage.jsx'
 import RootLayout from './components/RootLayout.jsx'
 import EventsPage, { loader as eventsLoader } from './pages/EventsPage.jsx'
 import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetailPage.jsx'
-import NewEventPage, { action as newEventAction } from './pages/NewEventPage.jsx'
+import NewEventPage from './pages/NewEventPage.jsx'
 import EditEventPage from './pages/EditEventPage.jsx'
 import EventsLayout from './pages/EventsLayout.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
+import { action as manipulateEventAction } from './components/EventForm.jsx'
+import NewsletterPage, { action as newsletterAction } from './pages/NewsletterPage.jsx'
 
 const router = createBrowserRouter([
 	{
@@ -30,13 +32,14 @@ const router = createBrowserRouter([
 						loader: eventDetailLoader,
 						children: [
 							{ index: true, element: <EventDetailPage />, action: deleteEventAction },
-							{ path: 'edit', element: <EditEventPage /> },
+							{ path: 'edit', element: <EditEventPage />, action: manipulateEventAction },
 						],
 					},
 
-					{ path: 'new', element: <NewEventPage />, action: newEventAction },
+					{ path: 'new', element: <NewEventPage />, action: manipulateEventAction },
 				],
 			},
+			{ path: 'newsletter', element: <NewsletterPage />, action: newsletterAction },
 		],
 	},
 ])
